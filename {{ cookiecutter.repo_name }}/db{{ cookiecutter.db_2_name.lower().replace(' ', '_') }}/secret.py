18b1edaac9fdc537387d196c47cd5f5a6614720f
TaruDesigns <taruelec@gmail.com>
2023-10-30 20:09:55 +0100

    Added secret.py base

---
+++ {{ cookiecutter.repo_name }}/db{{ cookiecutter.db_2_name.lower().replace(' ', '_') }}/secret.py
@@ -0,0 +1,8 @@
+# This file holds all the connection parameters.
+# Make sure you DON'T add this to source control!
+name="{{ cookiecutter.db_2_name}}"
+type="{{ cookiecutter.db_2_type}}"
+host="{{ cookiecutter.db_2_host}}"
+username="{{ cookiecutter.db_2_username}}"
+password="{{ cookiecutter.db_2_password}}"
+port="{{ cookiecutter.db_2_port}}"
