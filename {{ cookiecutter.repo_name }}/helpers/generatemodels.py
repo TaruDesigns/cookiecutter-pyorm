d47ad38f74841983af679ac54912490f00995f44
TaruDesigns <taruelec@gmail.com>
2023-10-31 19:24:01 +0100

    refactoring

--- {{ cookiecutter.repo_name }}/helpers/generatemodels.py
+++ {{ cookiecutter.repo_name }}/helpers/generatemodels.py
@@ -1,14 +1,17 @@
 from sqlalchemy import create_engine
 from sqlacodegen import CodeGenerator
-from .helpers import urlgenerator_postgres
 
 
-def create_models(dbtype: str, fileout: str):
+def create_models(connectionstring: str, fileout: str):
     # Create an SQLAlchemy engine to connect to your PostgreSQL database
-    engine = create_engine(urlgenerator_postgres())
+    if connectionstring is None:
+        raise ValueError("Empty connection string")
+    engine = create_engine(connectionstring)
     # Create a CodeGenerator instance and generate models
     generator = CodeGenerator(engine)
     models = generator.generate_code()
     # Print the generated models
+    if fileout is None:
+        fileout = "models.py"
     with open(fileout, "w") as f:
         f.write(models)
