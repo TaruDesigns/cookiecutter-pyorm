d991f859d6b322fa1989faee4792630feaad6fdd
TaruDesigns <taruelec@gmail.com>
2023-10-31 20:13:04 +0100

    Post gen venv stuff
    AutoVenv Tests
    
    Fixed createvenv
    
    Post gen project
    
    postgen
    
    Post Gen - don't try to make venv

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,8 @@
 # post_gen_project.py
+import subprocess
 import os
+import platform
+import sys
 import shutil
 
 from pathlib import Path
@@ -16,19 +19,27 @@ def remove(filepath):
     elif os.path.isdir(filepath):
         shutil.rmtree(filepath)
         
+def remove_db2():
+    # Check if user wants the folder
+    cookiecutter_var = "{{cookiecutter.add_db2}}"
+    db2folder_cookiecutter = "db{{ cookiecutter.db_2_name.lower().replace(' ', '_') }}"
+    remove_db2 = cookiecutter_var == "no"
+
+    # User does not want folder so remove it
+    if remove_db2:
+        folder_path = os.path.join(
+            parent_path, 
+            "{{ cookiecutter.repo_name }}", 
+            db2folder_cookiecutter
+        )
+        remove(folder_path)
+        print("Database 2 Files Removed!")
+    return remove_db2
 
 
-# Check if user wants the folder
-cookiecutter_var = "{{cookiecutter.add_db2}}"
-db2folder_cookiecutter = "db{{ cookiecutter.db_2_name.lower().replace(' ', '_') }}"
-add_folder = cookiecutter_var == "yes"
 
-# User does not want folder so remove it
-if not add_folder:
-    folder_path = os.path.join(
-        parent_path, 
-        "{{ cookiecutter.repo_name }}", 
-        db2folder_cookiecutter
-    )
-    print(folder_path)
-    remove(folder_path)
+if __name__ == "__main__":
+    print("Post Repo Creation Cleanup...")
+    remove_db2 = remove_db2()
+    print("Repo created! Make sure you check the README.md to init the models")
+    
